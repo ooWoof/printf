5a2e886eca36820814f46a053aa0fab153c6c29c
ooWoof <ricobricko7102@gmail.com>
2023-07-29 05:27:26 -0700

    printf

--- function.c
+++ function.c
@@ -159,7 +159,7 @@ int print_int(va_list types, char buffer[],
 int print_binary(va_list types, char buffer[],
                  int flags, int width, int precision, int size)
 {
-    unsigned int n, i;
+    unsigned int n, i, m, sum;
     unsigned int a[32];
     int count = 0;
 
@@ -167,23 +167,27 @@ int print_binary(va_list types, char buffer[],
     UNUSED(width);
     UNUSED(precision);
     UNUSED(size);
+    UNUSED(buffer);
+
 
     n = va_arg(types, unsigned int);
+    m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
 
-    for (i = 0; i < 32; i++)
-    {
-        a[i] = n & 0x80000000 ? '1' : '0';
-        n = n << 1;
-    }
-
-    for (i = 0; i < 32; i++)
-    {
-        if (a[i] == '1' || i == 31)
-        {
-            write(1, &a[i], 1);
-            count++;
-        }
-    }
-    return (count);
+			write(1, &z, 1);
+			count++;
+		}
+	}
+	return (count);
 }
-
